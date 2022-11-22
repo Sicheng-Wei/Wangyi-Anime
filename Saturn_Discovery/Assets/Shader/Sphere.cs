74dd8f8f6f91a8bac1f4590efd13f17cf06a6d3f
Sicheng Wei <2495665501@qq.com>
2022-11-22 10:44:34 +0800

    Skybox Added

--- Saturn_Discovery/Assets/Shader/Sphere.cs
+++ Saturn_Discovery/Assets/Shader/Sphere.cs
@@ -1,31 +1,118 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Sphere : MonoBehaviour
 {
-    private Vector3 centerPos;    //你围绕那个点 就用谁的角度
-    private float radius = 3;     //物理离 centerPos的距离
-    private float angle = 0;      //偏移角度  
+    MeshRenderer meshRenderer;
+    MeshFilter meshFilter;
 
+    List<Vector3> verts;
+    List<int> indices;
+    int N = 10;
     void Start()
     {
-        CreateSphere();
+        verts = new List<Vector3>();
+        indices = new List<int>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshFilter = GetComponent<MeshFilter>();
+        BuildModel();
     }
 
-
-    public void CreateSphere()
+    void BuildModel()
     {
-        centerPos = transform.position;
-        //20度生成一个圆
-        for (int i = 0; i < 1000; i++)
+        for (float i = -N / 2f; i <= N / 2f; i++)
         {
-            Vector3 p = Random.insideUnitSphere * radius;
-            Vector3 pos = p.normalized * (2 + p.magnitude);
-
-            GameObject obj1 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            //设置物体的位置Vector3三个参数分别代表x,y,z的坐标数  
-            obj1.transform.position = pos;
+            for (float j = -N / 2f; j <= N / 2f; j++)
+            {
+                verts.Add(new Vector3(i, j, -N / 2f));
+            }
         }
+        for (float i = -N / 2f; i <= N / 2f; i++)
+        {
+            for (float j = -N / 2f; j <= N / 2f; j++)
+            {
+                verts.Add(new Vector3(N / 2f, j, i));
+            }
+        }
+        for (float i = -N / 2f; i <= N / 2f; i++)
+        {
+            for (float j = -N / 2f; j <= N / 2f; j++)
+            {
+                verts.Add(new Vector3(i, N / 2f, j));
+            }
+        }
+        for (float i = -N / 2f; i <= N / 2f; i++)
+        {
+            for (float j = -N / 2f; j <= N / 2f; j++)
+            {
+                verts.Add(new Vector3(-N / 2f, j, i));
+            }
+        }
+        for (float i = -N / 2f; i <= N / 2f; i++)
+        {
+            for (float j = -N / 2f; j <= N / 2f; j++)
+            {
+                verts.Add(new Vector3(i, j, N / 2f));
+            }
+        }
+        for (float i = -N / 2f; i <= N / 2f; i++)
+        {
+            for (float j = -N / 2f; j <= N / 2f; j++)
+            {
+                verts.Add(new Vector3(i, -N / 2f, j));
+            }
+        }
+
+        //真正控制球体成型的部分
+        for (int i = 0; i < verts.Count; i++)
+        {
+            verts[i] = verts[i].normalized;
+        }
+        //绘制
+        MakePos(0);
+        MakePos(1);
+        MakePos(2);
+        OtherMakePos(3);
+        OtherMakePos(4);
+        OtherMakePos(5);
+        Draw();
+    }
+    public void MakePos(int num)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                int index = j * (N + 1) + (N + 1) * (N + 1) * num + i;
+                int up = (j + 1) * (N + 1) + (N + 1) * (N + 1) * num + i;
+                indices.AddRange(new int[] { index, index + 1, up + 1 });
+                indices.AddRange(new int[] { index, up + 1, up });
+            }
+        }
+    }
+    public void OtherMakePos(int num)
+    {
+        for (int i = 0; i < N + 1; i++)
+        {
+            for (int j = 0; j < N + 1; j++)
+            {
+                if (i != N && j != N)
+                {
+                    int index = j * (N + 1) + (N + 1) * (N + 1) * num + i;
+                    int up = (j + 1) * (N + 1) + (N + 1) * (N + 1) * num + i;
+                    indices.AddRange(new int[] { index, up + 1, index + 1 });
+                    indices.AddRange(new int[] { index, up, up + 1 });
+                }
+            }
+        }
+    }
+    public void Draw()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = verts.ToArray();
+        mesh.triangles = indices.ToArray();
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+        meshFilter.mesh = mesh;
     }
 }
