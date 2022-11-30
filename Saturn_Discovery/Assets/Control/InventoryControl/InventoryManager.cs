97325d2f59ac94817e51cc09d86933f990758e08
BonoZhou <32444973+BonoZhou@users.noreply.github.com>
2022-11-30 18:13:52 +0800

    更改了InventoryControl 增加了InventoryManager的内容，可以更改物品数目并反映到前端

--- Saturn_Discovery/Assets/Control/InventoryControl/InventoryManager.cs
+++ Saturn_Discovery/Assets/Control/InventoryControl/InventoryManager.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class InventoryManager : MonoBehaviour
 {
+    Dictionary<string, int> Inv = new Dictionary<string, int>
+{
+    {"55200" , 1},
+    {"4638" , 0},
+    {"9337" , 0},
+    {"9843" , 0},
+    {"4620" , 1},
+    {"4672" , 0},
+    {"4642" , 0},
+    {"4639" , 0},
+    {"4637" , 0},
+    {"4633" , 0},
+    {"4645" , 0},
+    {"4618" , 0}
+};
+    Button btn;
+
+    [SerializeField]
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +33,29 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    void changenum(string ID, int num)
+    {
+        Inv[ID] = num;
+        GameObject.Find(ID).GetComponent<InventoryControl>().updatenum(num);
+        
+    }
+
+    void addnum(string ID, int num)
+    {
+        Inv[ID] += num;
+        GameObject.Find(ID).GetComponent<InventoryControl>().updatenum(num);
+    }
+    int  minusnum(string ID, int num)
+    {
+        if (Inv[ID] > num)
+        {
+            Inv[ID] -= num;
+            GameObject.Find(ID).GetComponent<InventoryControl>().updatenum(num);
+            return 0;
+        }
+        else return 1;
     }
 }
