cec224fe3b5bac5febad14b231688bdfcec70097
QingYuanZi2020 <65812233+QingYuanZi2020@users.noreply.github.com>
2022-12-01 17:54:27 +0800

    add ui

--- Saturn_Discovery/Assets/Control/SceneExchange.cs
+++ Saturn_Discovery/Assets/Control/SceneExchange.cs
@@ -2,17 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
 
 public class SceneExchange : MonoBehaviour
 {
     public void StartPage()
     {
-        SceneManager.LoadScene("OutdoorsScene");
+        SceneManager.LoadScene("story1");
     }
 
     public void GamePage()
     {
         SceneManager.LoadScene("StartPage");
+    }
+
+    public void Story1()
+    {
+        SceneManager.LoadScene("story2");
+    }
+
+    public void Story2()
+    {
+        SceneManager.LoadScene("story3");
+    }
+
+    public void Story3()
+    {
+        SceneManager.LoadScene("story4");
+    }
+
+    public void Story4()
+    {
+        SceneManager.LoadScene("story5");
+    }
+
+    public void Story5()
+    {
+        SceneManager.LoadScene("story6");
+    }
+
+    public void Story6()
+    {
+        SceneManager.LoadScene("OutdoorsScene");
+    }
+
+    public void introduce()
+    {
+        //1
+        Application.OpenURL("https://github.com/Sicheng-Wei/Wangyi-Anime/blob/main/README.md");
+        //2
+        //Help.BrowseURL("https://github.com/Sicheng-Wei/Wangyi-Anime/blob/main/README.md");
+        //3
+        //System.Diagnostics.Process.Start("https://github.com/Sicheng-Wei/Wangyi-Anime/blob/main/README.md");
     }
 
     public void ExitGame()
