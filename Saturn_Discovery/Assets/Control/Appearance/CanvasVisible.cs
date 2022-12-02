24c3cc6cdbea6541f27071d0649fc36943994b50
Sicheng Wei <2495665501@qq.com>
2022-12-02 14:39:59 +0800

    Panel Hide

--- Saturn_Discovery/Assets/Control/Appearance/CanvasVisible.cs
+++ Saturn_Discovery/Assets/Control/Appearance/CanvasVisible.cs
@@ -1,8 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class CanvasVisible : MonoBehaviour
 {
-    
+    [SerializeField]
+    public RectTransform panel;
+    private bool isVisible = true;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            if (isVisible) panel.localScale = new Vector3(0, 0);
+            else panel.localScale = new Vector3(1, 1);
+
+            isVisible = !isVisible;
+        }
+    }
 }
